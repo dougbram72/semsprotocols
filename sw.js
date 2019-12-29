9100e659ad57d93ddb62654d01429238f8d51a93
Doug Brammer <dougbram@gmail.com>
2019-12-29 10:12:59 -0600

    Updates

---
+++ sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'protocol-cache-v6';
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(
+          [
+           
+            "https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+            "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
+            "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js",
+            "https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            
+          ]
+        );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open('mysite-dynamic').then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          return response || fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
