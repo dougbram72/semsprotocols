538769ed2ff65f77c3a2705c7326d56c7676cea7
dougbram72 <59339213+dougbram72@users.noreply.github.com>
2020-02-01 11:10:52 -0600

    Progress update

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ self.addEventListener('install', function(event) {
 
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.open('mysite-dynamic').then(function(cache) {
+      caches.open(cacheName).then(function(cache) {
         return cache.match(event.request).then(function (response) {
           return response || fetch(event.request).then(function(response) {
             cache.put(event.request, response.clone());
