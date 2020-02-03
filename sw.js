a37cd8f0229d6179f63720b395353d59b8a5d8ae
dougbram72 <59339213+dougbram72@users.noreply.github.com>
2020-02-03 16:50:21 -0600

    sw update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'protocol-cache-v4';
+const cacheName = 'protocol-cache-v5';
 self.addEventListener('install', function(event) {
   console.log(`sw installed ${cacheName}`);
     event.waitUntil(
@@ -10,8 +10,8 @@ self.addEventListener('install', function(event) {
             "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
             "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js",
             "https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",
-            "/semsprotocols/",
-            "/semsprotocols/*.*"
+            "/semsprotocols/"
+            
             
           ]
         );
