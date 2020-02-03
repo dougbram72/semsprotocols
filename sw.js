1310c103027d6a0cb3b31f07371464674f10abb1
dougbram72 <59339213+dougbram72@users.noreply.github.com>
2020-02-03 16:02:42 -0600

    sw update

--- sw.js
+++ sw.js
@@ -10,8 +10,7 @@ self.addEventListener('install', function(event) {
             "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js",
             "https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",
             "/semsprotocols/",
-            "/semsprotocols/abdominalinjuries.html",
-            "/semsprotocols/chestpain.html"
+            "/semsprotocols/*.*"
             
           ]
         );
